190c0535a00445f987ba63730f9ed41df51c1f4e
rksin8 <ranjeetbhu88@gmail.com>
2018-08-30 12:55:39 +1000

    getItem method added

--- ch3/InputDialog/main.cpp
+++ ch3/InputDialog/main.cpp
@@ -6,6 +6,8 @@
 #include <QObject>
 #include <QMessageBox>
 
+class QStringList;
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -25,6 +27,28 @@ int main(int argc, char *argv[])
         QMessageBox::information(0, "City Name",
                 QString("city name is %1").arg(text));
     }
+
+    ok = false;
+    QStringList items;
+    items << QObject::tr("Foo") << QObject::tr("Bar") << QObject::tr("Baz");
+
+    QString item = QInputDialog::getItem(
+            0,
+            QObject::tr("Item"),
+            QObject::tr("Pick an item:"),
+            items,
+            0,
+            false,
+            &ok);
+
+
+
+    if(ok && !item.isEmpty()){
+        QMessageBox::information(0, "Item",
+                QString("You picked %1").arg(item));
+    }
+
+
     
     return app.exec();
 }
